aac7ad2d5297dc4b4cc402a653b1cba23843be84
FedorGutsch <gucinskijfedor@gmail.com>
2026-05-13 13:19:36 +0300

    Начал писать базовую модель фильмов

--- backend/schemas/films.py
+++ backend/schemas/films.py
@@ -1,9 +1,28 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, field_validator
 from typing import Annotated, Optional, Literal
 
-class FilmBase(BaseModel):
-    pass
+from datetime import datetime
 
+
+class FilmBase(BaseModel):
+    title_ru: Annotated[str, Field(min_length=2, max_length=100, description="Название фильма на русском")]
+    title_en: Annotated[str, Field(min_length=2, max_length=100, description="Название фильма на английском")]
+    year: Annotated[int, Field(ge=1930, description='Год выпуска')]
+    rating_kp: Annotated[float, Field(ge=0.0, le=10.0, description='Рейтинг на кинопоиске')]
+    director: str
+    actors: Annotated[list[str], Field(description='Список актеров')]
+    
+    @field_validator("year", mode='after')
+    def year_validator(cls, value):
+        if value > datetime.now().year:
+            raise ValueError('Год выпуска должен быть меньше или равен чем текущий')
+        return value
+    
+    @field_validator('actors', mode='before')
+    def actor_to_list():
+        pass
+    
+    
 class FilmCreate(FilmBase):
     pass
 
