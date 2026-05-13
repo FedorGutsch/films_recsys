ef5417e2411fb1d243d8c6d4bb96f0490062b8ba
FedorGutsch <gucinskijfedor@gmail.com>
2026-05-13 12:24:23 +0300

    Переписал логику pydantic models

--- backend/schemas/films.py
+++ backend/schemas/films.py
@@ -1,2 +1,12 @@
+from pydantic import BaseModel
+from typing import Annotated, Optional, Literal
 
+class FilmBase(BaseModel):
+    pass
+
+class FilmCreate(FilmBase):
+    pass
+
+class FilmRead(FilmBase):
+    pass
     
